b5ce6c90f21f0d74867cd2fab98ffec0734e93e2
sumit-kumar70 <sumitkumarbxr11@gmail.com>
2026-05-14 23:31:47 +0530

    Remove unnecessary comments in ChessGame.jsx

--- ChessGame.jsx
+++ ChessGame.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect, useMemo } from 'react';
-
+// //
 const initialBoard = [
     [{type: 'r', color: 'b'}, {type: 'n', color: 'b'}, {type: 'b', color: 'b'}, {type: 'q', color: 'b'}, {type: 'k', color: 'b'}, {type: 'b', color: 'b'}, {type: 'n', color: 'b'}, {type: 'r', color: 'b'}],
     [{type: 'p', color: 'b'}, {type: 'p', color: 'b'}, {type: 'p', color: 'b'}, {type: 'p', color: 'b'}, {type: 'p', color: 'b'}, {type: 'p', color: 'b'}, {type: 'p', color: 'b'}, {type: 'p', color: 'b'}],
     Array(8).fill(null),
-    Array(8).fill(null), //
+    Array(8).fill(null), 
     Array(8).fill(null),
     Array(8).fill(null),
     [{type: 'p', color: 'w'}, {type: 'p', color: 'w'}, {type: 'p', color: 'w'}, {type: 'p', color: 'w'}, {type: 'p', color: 'w'}, {type: 'p', color: 'w'}, {type: 'p', color: 'w'}, {type: 'p', color: 'w'}],
